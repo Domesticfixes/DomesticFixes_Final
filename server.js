71aee5df14a6f729239c2e8d9092c705fc4dbfb6
Domesticfixes <domesticfixesie@gmail.com>
2024-12-15 17:28:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -47,20 +47,24 @@ Contact: ${handymanEmail}
 
 Service Details:
 Service Type: ${serviceType}
-Quote Amount: €${finalQuoteAmount}
 
 Your Details:
 Name: ${customerName}
 Contact: ${customerPhoneNumber}
 
+Payment Details:
+You have already paid a blocking fee of €20. The remaining amount (€${quoteAmount - 20}) should be paid directly to the handyman upon successful completion of the service.
+
+
 We recommend reaching out to your assigned handyman directly if you have specific requests or need to provide additional information. Your handyman will be well-prepared to meet your requirements on the scheduled day.
 
-Should you have any questions or if there’s anything further we can assist you with, please don't hesitate to contact us at domesticfixesie@gmail.com.
+Should you have any questions or if there’s anything further we can assist you with, please don't hesitate to contact us at domesticfixesie@gmail.com
 
 We appreciate your trust in DomesticFixes and look forward to providing you with outstanding service!
 
 Warm regards,  
-The DomesticFixes Team`,
+The DomesticFixes Team
+`,
     };
 
     // Email to the handyman
@@ -70,7 +74,7 @@ The DomesticFixes Team`,
       subject: "New Service Request Confirmed!",
       text: `Dear ${handymanName},
 
-We’re pleased to inform you that your quote for €${quoteAmount} has been accepted by the customer. Here are the details of the service request:
+We’re pleased to inform you that your quote  has been accepted by the customer. Here are the details of the service request:
 
 Customer Details:
 Name: ${customerName}
@@ -79,14 +83,17 @@ Request Information: ${jobDescription}
 
 Service Details:
 Service Type: ${serviceType}
-Scheduled Amount: €${finalQuoteAmount}
+Balance Due: €${quoteAmount - 20}
 
-Please make any necessary arrangements and prepare to provide exceptional service to our valued customer. If you have any questions or need additional details, feel free to reach out to us.
-
-Thank you for being a trusted partner with DomesticFixes. We look forward to helping you deliver excellent service.
-
+ A blocking fee of €20 has been deducted from your quote as per company policy. You will receive €${quoteAmount - 20} upon successful completion of the service.
+    
+Please make the necessary arrangements and prepare to provide exceptional service to our valued customer. If you have any questions or require further details, feel free to contact us.
+    
+Thank you for being a trusted partner with DomesticFixes. We value your professionalism and commitment to excellent service.
+    
 Best regards,  
-The DomesticFixes Team`,
+The DomesticFixes Team
+`,
     };
 
     // Send emails
